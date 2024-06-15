946042850edcd5e6521f63f4b19d4a5bd6085d2a
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-15 10:58:23 -0300

    feat: Criando o metodo: addCategory - CategoryController

--- graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
+++ graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
@@ -1,7 +1,24 @@
 package com.gabriel.souto.graphQl.apresentation;
 
+import com.gabriel.souto.graphQl.domain.model.category.Category;
+import com.gabriel.souto.graphQl.domain.model.category.request.CategoryInput;
+import com.gabriel.souto.graphQl.domain.repositorys.ICategoryRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CategoryController {
+    private final ICategoryRepository _categoryRepository;
+
+
+    public CategoryController(ICategoryRepository categoryRepository) {
+        _categoryRepository = categoryRepository;
+    }
+    @MutationMapping
+    Category addCategory(@Argument CategoryInput categoryInput){
+       return _categoryRepository.save(new Category(categoryInput.getName()));
+    }
+
+
 }
