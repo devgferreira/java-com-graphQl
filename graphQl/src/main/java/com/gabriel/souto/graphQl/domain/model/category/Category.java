72d6d4f5466e209c5c1a6ed3183b8b50a8425ecf
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-15 10:43:46 -0300

    feat: Criando a entidade Category

---
+++ graphQl/src/main/java/com/gabriel/souto/graphQl/domain/model/category/Category.java
@@ -0,0 +1,24 @@
+package com.gabriel.souto.graphQl.domain.model.category;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Category(String name){
+        this.name = name;
+    }
+}
