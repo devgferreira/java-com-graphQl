814f371d815a63d9df188eb0703e65a0fbaabe42
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-15 11:29:40 -0300

    feat: criando o método: addProduct e products - ProductController

--- graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/ProductController.java
+++ graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/ProductController.java
@@ -1,4 +1,31 @@
 package com.gabriel.souto.graphQl.apresentation;
 
+import com.gabriel.souto.graphQl.domain.model.product.Product;
+import com.gabriel.souto.graphQl.domain.model.product.input.ProductInput;
+import com.gabriel.souto.graphQl.domain.repositorys.IProductRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.Iterator;
+import java.util.List;
+
+@Controller
 public class ProductController {
+
+    private final IProductRepository _productRepository;
+
+    public ProductController(IProductRepository productRepository) {
+        _productRepository = productRepository;
+    }
+    @MutationMapping
+    Product addProduct(@Argument ProductInput productInput){
+        return _productRepository.save(new Product(productInput.getName(),
+                productInput.getPrice(), productInput.getCategoryId()));
+    }
+    @QueryMapping()
+    List<Product> products(){
+        return _productRepository.findAll();
+    }
 }
