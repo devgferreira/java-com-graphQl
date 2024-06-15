bd5a8b584b3a0d591463614002d80afd4203b394
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-15 11:11:36 -0300

    feat: mudando o parametro do método: addCategory - CategoryController

--- graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
+++ graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
@@ -5,7 +5,10 @@ import com.gabriel.souto.graphQl.domain.model.category.request.CategoryInput;
 import com.gabriel.souto.graphQl.domain.repositorys.ICategoryRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class CategoryController {
@@ -19,6 +22,10 @@ public class CategoryController {
     Category addCategory(@Argument CategoryInput categoryInput){
        return _categoryRepository.save(new Category(categoryInput.getName()));
     }
+   @QueryMapping
+    Optional<Category> categoryById(@Argument Integer id){
+        return _categoryRepository.findById(id);
+   }
 
 
 }
