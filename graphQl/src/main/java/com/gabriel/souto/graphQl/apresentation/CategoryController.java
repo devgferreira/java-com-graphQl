f38badd213a0452e413bbe1aad18c08ed9085626
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-15 11:30:08 -0300

    feat: criando a query e o mutation para o Product - schema.graphqls

--- graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
+++ graphQl/src/main/java/com/gabriel/souto/graphQl/apresentation/CategoryController.java
@@ -1,16 +1,16 @@
 package com.gabriel.souto.graphQl.apresentation;
 
 import com.gabriel.souto.graphQl.domain.model.category.Category;
-import com.gabriel.souto.graphQl.domain.model.category.request.CategoryInput;
+import com.gabriel.souto.graphQl.domain.model.category.input.CategoryInput;
 import com.gabriel.souto.graphQl.domain.repositorys.ICategoryRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import java.util.Optional;
 
-@RestController
+@Controller
 public class CategoryController {
     private final ICategoryRepository _categoryRepository;
 
